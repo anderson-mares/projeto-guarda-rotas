4afea0b5dbcce3ac5b7d98379f48ce8ba7bcebf2
Anderson Alves de Oliveira <andersonmares@gmail.com>
2024-11-16 19:57:32 -0300

    Gerando a estrutura inicial

---
+++ desafio-advocacia/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { CarouselComponent } from "./carousel/carousel.component";
+import { ContactFormComponent } from "./contact-form/contact-form.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HeaderComponent, CarouselComponent, ContactFormComponent, AboutUsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'desafio-advocacia';
+}
